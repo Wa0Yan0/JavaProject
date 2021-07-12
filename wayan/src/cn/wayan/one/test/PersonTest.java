d8e47b0016c92a97f0aa17bb54055b4ff2e649a2
wayan <1790484858@qq.com>
2021-07-12 13:03:23 +0800

    ok

--- wayan/src/cn/wayan/one/test/PersonTest.java
+++ wayan/src/cn/wayan/one/test/PersonTest.java
@@ -9,11 +9,6 @@ public class PersonTest {
         Person person = Person.class.getDeclaredConstructor().newInstance();
         Person person2 = Person.class.getDeclaredConstructor().newInstance();
         System.out.println(person);
-        System.out.println("合并分支");
-        System.out.println("master test");
-        System.out.println("hot-fix test");
-        System.out.println("push test");
-        System.out.println("pull test");
-        System.out.println("Mac test");
+        System.out.println("测试完毕");
     }
 }
