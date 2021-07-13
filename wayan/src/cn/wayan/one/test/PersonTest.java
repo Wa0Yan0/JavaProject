6718381c8a3aa7e91d8ce00cf1aa88b4f004485f
wayanMac <coolboywayan@gmail.com>
2021-07-13 17:52:56 +0800

    add query

--- wayan/src/cn/wayan/one/test/PersonTest.java
+++ wayan/src/cn/wayan/one/test/PersonTest.java
@@ -1,45 +1,21 @@
 package cn.wayan.one.test;
 
+import cn.wayan.util.JDBCUtils;
 import org.junit.Test;
 
 import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.Driver;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.util.Properties;
 
 public class PersonTest {
 
     @Test
     public void test() {
-        Connection conn = null;
-        PreparedStatement ps = null;
-        try{
-            conn = JDBCUtils.getConnection();
-            System.out.println(conn);
-
-            /**
-             * 预编译sql语句
-             */
-            String sql = "update customers set name = ? where id = ?";
-            ps = conn.prepareStatement(sql);
-
-            /**
-             * 填充数据
-             */
-            ps.setObject(1,"高勋");
-            ps.setObject(2,18);
-
-            /**
-             * 执行
-             */
-            ps.execute();
-            System.out.println("跟新成功");
-        }catch (Exception e){
-            e.printStackTrace();
-        }finally {
-            JDBCUtils.closeResource(conn,ps);
-        }
-
+        String sql = "select id,name,email,birth from customers where id = ?";
+        customers cus = JDBCUtils.queryForCustomer(sql, 19);
+        System.out.println(cus);
     }
 }
--- wayan/src/cn/wayan/util/JDBCUtils.java
+++ wayan/src/cn/wayan/util/JDBCUtils.java
@@ -1,17 +1,17 @@
 package cn.wayan.util;
 
+import cn.wayan.one.test.customers;
+import com.mysql.cj.protocol.Resultset;
+
 import java.beans.Statement;
 import java.io.InputStream;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.lang.reflect.Field;
+import java.sql.*;
 import java.util.Properties;
 
 public class JDBCUtils {
     /**
      * 获取数据库连接
-     *
      * @return Connection conn
      * @throws Exception
      */
@@ -42,7 +42,7 @@ public class JDBCUtils {
     }
 
     /**
-     * 释放资源
+     * 二参数释放资源
      * @param conn
      * @param ps
      */
@@ -61,4 +61,103 @@ public class JDBCUtils {
         }
 
     }
+
+    /**
+     * 三参数释放资源
+     * @param conn
+     * @param ps
+     * @param rs
+     */
+    public static void closeResource(Connection conn, PreparedStatement ps, ResultSet rs) {
+        try {
+            if (conn != null)
+                conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        try {
+            if (ps != null)
+                ps.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        try {
+            if (rs != null)
+                rs.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    /**
+     * 通用增删改
+     * @param sql
+     * @param Object...args - 可变形参长度
+     */
+    public static void modifySql(String sql, Object...args){
+        Connection conn = null;
+        PreparedStatement ps = null;
+        try {
+            // 连接数据库
+            conn = getConnection();
+            // 预编译sql语句
+            ps = conn.prepareStatement(sql);
+            // 填充数据
+            for (int i = 0; i < args.length; i++) {
+                ps.setObject(i+1,args[i]);
+            }
+            // 执行
+            ps.execute();
+            System.out.println("数据库跟新成功！！！");
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            // 释放资源
+            closeResource(conn,ps);
+        }
+    }
+
+    public static customers queryForCustomer(String sql, Object...args){
+        Connection conn = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        try {
+            conn = getConnection();
+            ps = conn.prepareStatement(sql);
+            // 填充数据
+            for (int i = 0; i < args.length; i++) {
+                    ps.setObject(i+1,args[i]);
+            }
+            // 查询后得到结果集
+            rs = ps.executeQuery();
+            // 获取结果集的元数据
+            ResultSetMetaData metaData = rs.getMetaData();
+            // 通过元数据获取结果集的列数
+            int columnCount = metaData.getColumnCount();
+            if(rs.next()){
+                // 实例化一个空参构造函数，通过存在的列名，设置属性值
+                customers cus = new customers();
+                for (int i = 0; i < columnCount; i++) {
+                    // 获取属性值
+                    Object value = rs.getObject(i + 1);
+                    // 获取列名
+                    String columnClassName = metaData.getColumnName(i + 1);
+                    // 通过反射，将列名变成对象
+                    Field field = customers.class.getDeclaredField(columnClassName);
+                    // 私有属性，设置访问权限
+                    field.setAccessible(true);
+                    // 设置属性值
+                    field.set(cus,value);
+                }
+                return cus;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            closeResource(conn,ps,rs);
+        }
+        return null;
+    }
+
 }
